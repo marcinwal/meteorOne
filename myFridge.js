abdb35fb1ddc3cc8ac441a8cd6e700441d945671
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-06 21:18:56 +0100

    adding styling and DB on the server side init

--- myFridge.js
+++ myFridge.js
@@ -1,23 +1,35 @@
+Products = new Mongo.Collection('products');
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  // Session.setDefault('counter', 0);
+  Template.fridge.helpers({
+    products: function(){
+      return Products.find({place: 'fridge'});
+    }   
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.productList.helpers({
+    products: function(){
+      return Products.find({place: 'supermarket'});
     }
-  });
+  });  
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  Products.remove({});
+  Products.insert({
+    name: 'Milk',
+    img: '/milk.png',
+    place: 'fridge'
+  });  
+  Products.insert({
+    name: 'Bread',
+    img: '/bread.png',
+    place: 'supermarket'
+  });
+
   });
 }
